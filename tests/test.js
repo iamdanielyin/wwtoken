c164438f36eb2883d456b935d29c7780f0410719
yinfxs <yinfxs@gmail.com>
2017-09-13 14:23:01 +0800

    fix bug

--- tests/test.js
+++ tests/test.js
@@ -5,29 +5,29 @@ const config = {
     logsDir: 'test-logs',
     configs: [
         {
-            corpid: 'wxab1d81062855a83f',
-            corpsecret: 'L9HIQbaMYxCwKaPV-PwKhCylo6nE8sojH9rMjIi1wp2WA3vP5Akhf_nYMunIhVT6',
-            token_key: 'k11_access_token',
+            corpid: 'xxx',
+            corpsecret: 'xxx',
+            token_key: 'access_token_1',
             token_expires: 5,
-            ticket_key: 'k11_jsapi_ticket',
+            ticket_key: 'jsapi_ticket',
             ticket_expires: 7
         },
         {
-            corpid: 'wxab1d81062855a83f',
-            corpsecret: 'W3tDJ97wR93a2fKw3fv5BpQrMF_qFm8MgOSWbldCF4k',
-            token_key: 'k11_access_token_central',
+            corpid: 'xxx',
+            corpsecret: 'xxx',
+            token_key: 'access_token_2',
             token_expires: 6
         },
         {
-            corpid: 'wxab1d81062855a83f',
-            corpsecret: '9qAON_9w9auuW-uPKnXdVmrLIe-cv8FYyadUeyipCiQ',
-            token_key: 'k11_access_token_myscust',
+            corpid: 'xxx',
+            corpsecret: 'xxx',
+            token_key: 'access_token_3',
             token_expires: 7000
         },
         {
-            corpid: 'wxab1d81062855a83f',
-            corpsecret: '2h0vxBxriyEHnX85PNkYvTxINvwHhCjzx68xwQ6Q2B0',
-            token_key: 'k11_access_token_tour',
+            corpid: 'xxx',
+            corpsecret: 'xxx',
+            token_key: 'access_token_4',
             token_expires: 7000
         }
     ]
