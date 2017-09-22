3314fee12790c883cb2639c0afad5902b73fd810
yinfxs <yinfxs@gmail.com>
2017-09-22 11:11:17 +0800

    fix bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: yinfxs 
  * @Date: 2017-08-26 14:13:01 
  * @Last Modified by: yinfxs
- * @Last Modified time: 2017-09-21 22:44:52
+ * @Last Modified time: 2017-09-22 11:08:55
  */
 
 const path = require('path');
@@ -158,7 +158,7 @@ app.fetchToken = async (corpid, corpsecret, name) => {
     logger.log('Getting the latest token...');
     if (!corpid || !corpsecret) {
         logger.error(`Configuration error: 'corpid' and 'corpsecret' can not be empty`);
-        return setTimeout(process.exit, 500);
+        return;
     }
     const key = `${corpid}|${corpsecret}|${name}`;
     errcaches_token[key] = errcaches_token[key] || 0;
@@ -201,7 +201,7 @@ app.fetchTicket = async (access_token, name) => {
     logger.log('Getting a new ticket...');
     if (!access_token) {
         logger.error(`Configuration error: 'access_token' can not be empty`);
-        return setTimeout(process.exit, 500);
+        return;
     }
     const key = `${access_token}|${name}`;
     errcaches_ticket[key] = errcaches_ticket[key] || 0;
@@ -242,8 +242,8 @@ function onTokenError({ corpid, corpsecret, name, key, errcaches_token }) {
     errcaches_token[key]++;
     if (errcaches_token[key] <= 100) {
         return app.fetchToken(corpid, corpsecret, name);
-    } else {
-        return setTimeout(process.exit, 500);
+    }else{
+        logger.error(`Get${name ? ` '${name}' ` : ' '}token failed more than 100 times, please check the configuration.`);
     }
 }
 
@@ -255,8 +255,8 @@ function onTicketError({ access_token, name, key, errcaches_ticket }) {
     errcaches_ticket[key]++;
     if (errcaches_ticket[key] <= 100) {
         return app.fetchTicket(access_token, name);
-    } else {
-        return setTimeout(process.exit, 500);
+    }else{
+        logger.error(`Get${name ? ` '${name}' ` : ' '}ticket failed more than 100 times, please check the configuration.`);
     }
 }
 
@@ -272,6 +272,5 @@ app.updateRedis = async (key, value, ms) => {
         await redis.pipeline().del(key).set(key, value, 'EX', ms).exec();
     } catch (e) {
         logger.error(`Update value to redis failure: ${e}`);
-        return setTimeout(process.exit, 500);
     }
 };
